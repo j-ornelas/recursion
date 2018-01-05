9931f6d67e435d5a975cb9e5eed684d05787e1de
j-ornelas <johnnyoh1988@gmail.com>
2018-01-04 16:52:34 -0800

    Makes stringifyJSON match return of JSON.stringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,15 +4,34 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-	if (typeof obj === 'string'){
+    if (typeof obj === 'string'){
 		return '"' + obj + '"';
+	} else if (obj === null){	
+		return 'null'	
 	} else if (Array.isArray(obj)){
-	  var newArray = obj.slice(0);
-	  var stringArray = [];
-      for (i=0;i<newArray.length;i++){
-        stringArray.push(stringifyJSON(newArray[i]));
-      }
-      return '[' + stringArray + ']';
+	    var stringArray = [];
+	    for (i=0;i<obj.length;i++){
+	      stringArray.push(stringifyJSON(obj[i]));
+	    }
+	    return '[' + stringArray + ']';
+	} else if (typeof obj === 'object'){
+  	  if (Object.keys(obj).length === 0){
+  	    return '{}';
+  	  } else {
+  	    var stringObj = "{";
+  	    for (var key in obj){
+  	      if (typeof key === 'function' || typeof obj[key] === 'function' ||
+  	      	typeof key === undefined || typeof obj[key] === undefined){
+  	      	return '{}'
+  	      } else {
+     	    stringObj += stringifyJSON(key);
+  	        stringObj += ':';
+  	        stringObj += stringifyJSON(obj[key]);
+  	        stringObj += ",";
+  	      }
+  	    }
+  	  }
+	return stringObj.substring(0, stringObj.length - 1) + "}";
 	} else {
 		return '' + obj  + '';
 	}
