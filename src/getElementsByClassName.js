33fca1225235da2298c78b9169043c9901cd37d4
j-ornelas <johnnyoh1988@gmail.com>
2018-01-06 21:02:16 -0800

    makes getElementsByClassName act like og func

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,10 +1,22 @@
 // If life was easy, we could just do things the easy way:
 // var getElementsByClassName = function (className) {
-//   return document.getElementsByClassName(className);
+//   return JSON.stringify(document.getElementsByClassName(className));
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var matches =  [];
+  var check = function(node){
+    for (var i=0;i<node.childNodes.length;i++){
+    	if (node.childNodes[i].childNodes.length>0){
+    		check(node.childNodes[i]);
+    	}
+    	if (node.childNodes[i].getAttribute && node.childNodes[i].getAttribute('class') 
+    		&& node.childNodes[i].getAttribute('class').split(" ").indexOf(className) >= 0){
+    		matches.unshift(node.childNodes[i])
+    	}
+    }
+  }
+  check(document)
+  return matches
 };
